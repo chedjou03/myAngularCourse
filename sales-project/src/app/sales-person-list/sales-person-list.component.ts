df886335552549d09a7e81b2593f1242b4d9fbd3
chedjou03 <chedjou03@yahoo.fr>
2020-03-22 21:56:55 -0500

    first commit for the sale person list angular project

---
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  //create an arrays of salePerson objects
+  salesPersonList : SalesPerson[] = [
+    new SalesPerson ("Simplice","Chedjou","chedjou@yahoo.fr",40000),
+    new SalesPerson ("Jean Pierre","Fossi","jeanpierre@yahoo.com",50000),
+    new SalesPerson ("Christance","Nekame","neckChristanc@yahoo.fr",60000),
+    new SalesPerson ("Samuel","Kenmogne","samuel@yahoo.com",80000),
+    new SalesPerson ("Clotilde","Kengne","clotildeKengne@gmail.com",20000),
+    new SalesPerson ("Florence","Kuissi","florenceKuissu@yahoo.fr",60000 )
+  ];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
